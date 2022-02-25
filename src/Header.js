2da75f62f5b235a39a8f75d8a0541bc9cfd4e180
Christine <calyston.14@gmail.com>
2022-02-25 15:08:08 +0000

    feat: dark mode toggle functional

--- src/Header.js
+++ src/Header.js
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import parse from 'html-react-parser';
 import toggleon from './toggle-on.png';
 import toggleoff from './toggle-off.png';
 import './App.css';
 
 function Header() {
+  const [theme, setTheme] = useState('light-mode');
+
+  const toggleTheme = () => {
+    if (theme === 'light-mode') {
+      setTheme('dark-mode')
+    } else {
+      setTheme('light-mode')
+    }
+  }
+  useEffect(() => {
+    document.documentElement.className = theme
+  }, [theme])
+
   return (
     <header>
       <nav>
         <section className="nav-container">
           <h3>Dark Mode</h3>
-          <button className="toggle-button"><img id="toggle" src={toggleoff} alt="toggle" /></button>
+          <button className="toggle-button" onClick={toggleTheme}><img id="toggle" src={toggleoff} alt="toggle" /></button>
         </section>
       </nav>
     </header>
