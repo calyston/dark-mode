d229c6effd1c7e8430961f41223ead30bbb99579
Christine <calyston.14@gmail.com>
2022-02-25 14:12:10 +0000

    feat: moment js installed to display date

--- src/Article.js
+++ src/Article.js
@@ -1,11 +1,11 @@
 import React from 'react';
-// import moment from 'moment';
+import moment from 'moment';
 
 const Article = ({ title, snippet, date, length }) => {
   return (
     <article>
       <h2>{title}</h2>
-      <p>date</p>
+      <p>{moment(date).format('dddd Do MMMM YYYY')}</p>
       <p>{length} minute read</p>
       <p>{snippet}</p>
     </article>
